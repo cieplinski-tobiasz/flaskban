a888bf3ccd8bcab3fbb73e2be8d05fb498b42573
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2019-01-07 12:58:08 +0100

    Incorporate application factory pattern
    Cleaning up the code - the pattern allows to put extensions in one place, and simplifies applying different configurations.

--- flaskban-server/app.py
+++ flaskban-server/app.py
@@ -1,4 +1,5 @@
 import config
+from extensions import api, swagger
 
 from resources.auth import Login, Register
 from resources.collections import Boards, Columns, Tasks
@@ -6,30 +7,28 @@ from resources.entities import Board, Column, Task
 from resources.perms import Permissions, UserPermissions
 
 from flask import Flask
-from flask_restful import Api
-from flasgger import Swagger
 
-app = Flask(__name__)
-app.config.from_object(config.DevelopmentConfig)
 
-swag = Swagger(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config.DevelopmentConfig)
 
-api = Api(app)
+    api.init_app(app)
+    swagger.init_app(app)
 
-api.add_resource(Login, '/auth/login')
-api.add_resource(Register, '/auth/register')
+    api.add_resource(Login, '/auth/login')
+    api.add_resource(Register, '/auth/register')
 
-api.add_resource(Boards, '/boards')
-api.add_resource(Board, '/boards/<int:board_id>')
+    api.add_resource(Boards, '/boards')
+    api.add_resource(Board, '/boards/<int:board_id>')
 
-api.add_resource(Columns, '/boards/<int:board_id>/columns')
-api.add_resource(Column, '/boards/<int:board_id>/columns/<int:column_id>')
+    api.add_resource(Columns, '/boards/<int:board_id>/columns')
+    api.add_resource(Column, '/boards/<int:board_id>/columns/<int:column_id>')
 
-api.add_resource(Tasks, '/boards/<int:board_id>/tasks')
-api.add_resource(Task, '/boards/<int:board_id>/tasks/<int:task_id>')
+    api.add_resource(Tasks, '/boards/<int:board_id>/tasks')
+    api.add_resource(Task, '/boards/<int:board_id>/tasks/<int:task_id>')
 
-api.add_resource(Permissions, '/permissions')
-api.add_resource(UserPermissions, '/boards/<int:board_id>/permissions/<int:user_id>')
+    api.add_resource(Permissions, '/permissions')
+    api.add_resource(UserPermissions, '/boards/<int:board_id>/permissions/<int:user_id>')
 
-if __name__ == '__main__':
-    app.run()
+    return app
