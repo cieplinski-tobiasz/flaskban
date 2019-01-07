2f53cde511771bf5d92c86c9874fdabfd01ae2e5
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2019-01-07 20:47:35 +0100

    Add marshmallow schemas for User
    Several very minor changes included:
      - different name of username field to name - just a stylistic issue.
      - is None changed to not - more pythonic way

--- flaskban-server/models/users.py
+++ flaskban-server/models/users.py
@@ -1,13 +1,15 @@
-from common.errors import InvalidDataError, AlreadyExistsError
-from extensions import db
+from marshmallow import Schema, fields, post_load, validates_schema, ValidationError
 from passlib.hash import pbkdf2_sha256
 from sqlalchemy import exists
 from sqlalchemy.ext.hybrid import hybrid_property
 
+from common.errors import AlreadyExistsError, InvalidDataError
+from extensions import db
+
 
 class User(db.Model):
     id_ = db.Column('id', db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
+    name = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     _password = db.Column(db.String(50), nullable=False)
 
@@ -19,15 +21,23 @@ class User(db.Model):
     def password(self, plain):
         self._password = pbkdf2_sha256.hash(plain)
 
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email)
+
     def _already_saved(self, session):
         return session.query(
             exists().where(
-                (User.username == self.username) | (User.email == self.email)
+                (User.name == self.name) | (User.email == self.email)
             )
         ).scalar()
 
     def save(self):
-        if self.username is None or self._password is None or self.email is None:
+        if not self.name or not self._password or not self.email:
             raise InvalidDataError('Required fields are not present')
 
         if self._already_saved(db.session):
@@ -37,4 +47,38 @@ class User(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return f'User(id={self.id_}, username={self.username}, email={self.email})'
+        return f'User(id={self.id_}, name={self.name}, email={self.email})'
+
+
+class UserRegisterSchema(Schema):
+    __model__ = User
+
+    id_ = fields.Integer(attribute='id', dump_only=True)
+    username = fields.Str(attribute='name', required=True)
+    email = fields.Email(required=True)
+    password = fields.Str(load_only=True, required=True)
+
+    @post_load
+    def make_user(self, data):
+        return self.__model__(**data)
+
+
+class UserLoginSchema(Schema):
+    __model__ = User
+
+    username = fields.Str(attribute='name')
+    email = fields.Email()
+    password = fields.Str(attribute='_password', load_only=True, required=True)
+
+    @validates_schema
+    def validate(self, data):
+        if 'name' not in data and 'email' not in data:
+            raise ValidationError('Either username or email must be present')
+
+    @post_load
+    def make_user(self, data):
+        return self.__model__(**data)
+
+
+register_schema = UserRegisterSchema()
+login_schema = UserLoginSchema()
--- flaskban-server/test/models/test_users.py
+++ flaskban-server/test/models/test_users.py
@@ -1,20 +1,21 @@
 import unittest.mock as mock
 
 from common.errors import AlreadyExistsError, InvalidDataError
-from models.users import User
+from models.users import User, UserRegisterSchema, UserLoginSchema
 from unittest import TestCase
+from marshmallow.exceptions import ValidationError
 
 
 class UserTest(TestCase):
     def test_already_saved_calls_query(self):
-        uut = User(username='john', password='pwd', email='john.smith@gmail.com')
+        uut = User(name='john', password='pwd', email='john.smith@gmail.com')
         session_mock = mock.Mock()
 
         uut._already_saved(session_mock)
         session_mock.query.assert_called()
 
     def test_already_saved_returns_called_scalar(self):
-        uut = User(username='john', password='pwd', email='john.smith@gmail.com')
+        uut = User(name='john', password='pwd', email='john.smith@gmail.com')
         session_mock = mock.Mock()
         scalar_mock = mock.Mock()
         scalar_mock.scalar.return_value = True
@@ -25,7 +26,7 @@ class UserTest(TestCase):
         self.assertTrue(result)
 
     def test_save_calls_db(self):
-        uut = User(username='john', password='pwd', email='john.smith@gmail.com')
+        uut = User(name='john', password='pwd', email='john.smith@gmail.com')
         uut._already_saved = mock.Mock(return_value=False)
 
         with mock.patch('models.users.db') as mocked_db:
@@ -35,36 +36,117 @@ class UserTest(TestCase):
 
     def test_raises_user_already_exists(self):
         with self.assertRaises(AlreadyExistsError):
-            uut = User(username='present', password='pwd', email='already@exists.com')
+            uut = User(name='present', password='pwd', email='already@exists.com')
             uut._already_saved = mock.Mock(return_value=True)
 
             uut.save()
 
     @mock.patch('models.users.pbkdf2_sha256')
-    def test_passwords_are_hashed(self, mocked_pbkdf2):
-        User(username='dr. who', password='pwd', email='john.smith@gmail.com')
+    def test_init_hashes_password_kwarg(self, mocked_pbkdf2):
+        User(name='dr. who', password='pwd', email='john.smith@gmail.com')
         mocked_pbkdf2.hash.assert_called_with('pwd')
 
     @mock.patch('models.users.pbkdf2_sha256')
-    def test_password_getter_returns_hash(self, mocked_pbkdf2):
-        mocked_pbkdf2.hash.return_value = 'hash'
-        uut = User(username='dr. who', password='pwd', email='john.smith@gmail.com')
+    def test_init_does_not_hash_underscore_password_kwarg(self, mocked_pbkdf2):
+        uut = User(name='dr. who', _password='pwd', email='john.smith@gmail.com')
 
-        hashed_pwd = uut.password
+        mocked_pbkdf2.hash.assert_not_called()
+        self.assertEqual('pwd', uut.password)
 
-        self.assertEqual(hashed_pwd, 'hash')
-
-    def test_save_raises_when_username_missing(self):
+    def test_save_raises_when_name_missing(self):
         with self.assertRaises(InvalidDataError):
             uut = User(password='pwd', email='john.smith@gmail.com')
             uut.save()
 
     def test_save_raises_when_password_missing(self):
         with self.assertRaises(InvalidDataError):
-            uut = User(username='john', email='john.smith@gmail.com')
+            uut = User(name='john', email='john.smith@gmail.com')
             uut.save()
 
     def test_save_raises_when_email_missing(self):
         with self.assertRaises(InvalidDataError):
-            uut = User(username='john', password='pwd')
+            uut = User(name='john', password='pwd')
             uut.save()
+
+
+class UserRegisterSchemaTest(TestCase):
+    def test_load_raises_when_username_missing(self):
+        with self.assertRaises(ValidationError):
+            uut = UserRegisterSchema()
+            uut.load({'email': 'test@ab.com', 'password': 'pwd'})
+
+    def test_load_raises_when_email_missing(self):
+        with self.assertRaises(ValidationError):
+            uut = UserRegisterSchema()
+            uut.load({'username': 'john', 'password': 'pwd'})
+
+    def test_load_raises_when_password_missing(self):
+        with self.assertRaises(ValidationError):
+            uut = UserRegisterSchema()
+            uut.load({'username': 'john', 'email': 'test@ab.com'})
+
+    def test_loads_raises_when_invalid_email(self):
+        with self.assertRaises(ValidationError):
+            uut = UserRegisterSchema()
+            uut.load({'username': 'john', 'password': 'pwd', 'email': 'testab.com'})
+
+    def test_loads_raises_when_extra_data(self):
+        with self.assertRaises(ValidationError):
+            uut = UserRegisterSchema()
+            uut.load({'username': 'john', 'password': 'pwd', 'email': 'test@ab.com', 'extra': 'data'})
+
+    def test_loads_returns_correct_user_with_hashed_password(self):
+        uut = UserRegisterSchema()
+
+        result = uut.load({'username': 'john', 'password': 'pwd', 'email': 'test@ab.com'})
+
+        self.assertEqual(result.name, 'john')
+        self.assertEqual(result.email, 'test@ab.com')
+        self.assertNotEqual(result.password, 'pwd')
+
+
+class UserLoginSchemaTest(TestCase):
+    def test_load_raises_when_username_and_email_missing(self):
+        with self.assertRaises(ValidationError):
+            uut = UserLoginSchema()
+            uut.load({'password': 'pwd'})
+
+    def test_load_raises_when_password_missing(self):
+        with self.assertRaises(ValidationError):
+            uut = UserLoginSchema()
+            uut.load({'username': 'john', 'email': 'test@ab.com'})
+
+    def test_loads_raises_when_invalid_email(self):
+        with self.assertRaises(ValidationError):
+            uut = UserLoginSchema()
+            uut.load({'username': 'john', 'password': 'pwd', 'email': 'testab.com'})
+
+    def test_loads_raises_when_extra_data(self):
+        with self.assertRaises(ValidationError):
+            uut = UserLoginSchema()
+            uut.load({'username': 'john', 'password': 'pwd', 'email': 'test@ab.com', 'extra': 'data'})
+
+    def test_loads_returns_correct_user_with_not_hashed_password(self):
+        uut = UserLoginSchema()
+
+        result = uut.load({'username': 'john', 'password': 'pwd', 'email': 'test@ab.com'})
+
+        self.assertEqual(result.name, 'john')
+        self.assertEqual(result.email, 'test@ab.com')
+        self.assertEqual(result.password, 'pwd')
+
+    def test_loads_returns_correct_user_when_only_username_given(self):
+        uut = UserLoginSchema()
+
+        result = uut.load({'username': 'john', 'password': 'pwd'})
+
+        self.assertEqual(result.name, 'john')
+        self.assertEqual(result.password, 'pwd')
+
+    def test_loads_returns_correct_user_when_only_email_given(self):
+        uut = UserLoginSchema()
+
+        result = uut.load({'username': 'john', 'password': 'pwd', 'email': 'test@ab.com'})
+
+        self.assertEqual(result.email, 'test@ab.com')
+        self.assertEqual(result.password, 'pwd')
