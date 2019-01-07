0160edc54bfd6874ce56920d2da236da665cf4bb
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2019-01-07 15:46:08 +0100

    Add more common errors with base class

--- flaskban-server/common/errors.py
+++ flaskban-server/common/errors.py
@@ -1,2 +1,11 @@
-class InvalidDataError(Exception):
+class ClientError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class InvalidDataError(ClientError):
+    pass
+
+
+class AlreadyExistsError(ClientError):
     pass
