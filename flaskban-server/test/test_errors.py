2c12ee5f9aacd4e047efc0a7c5bc9f6245a37d25
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2019-01-12 20:05:44 +0100

    Add tests for handle_error function

---
+++ flaskban-server/test/test_errors.py
@@ -0,0 +1,71 @@
+"""
+The module contains unit tests for functions contained in 'error' module.
+"""
+
+import unittest.mock as mock
+from unittest import TestCase
+
+from errors import handle_error
+
+
+class HandleErrorTest(TestCase):
+    """
+    Test case for handle_error decorator.
+    """
+
+    def test_calls_fun_when_no_error(self):
+        """
+        Tests if wrapper returns wrapped function's return value.
+        """
+        fun = mock.Mock(return_value='success')
+        error_mock = mock.Mock()
+
+        result = handle_error(error_mock)(fun)()
+
+        self.assertEqual(result, 'success')
+
+    def test_returns_defined_message_on_error_exception_no_message(self):
+        """
+        Tests if wrapper returns error response with message equal to argument message,
+        when exception has no attribute named 'message'.
+        """
+        fun = mock.Mock(side_effect=ValueError)
+
+        error_response, _ = handle_error(ValueError, message='caught')(fun)()
+
+        self.assertEqual(error_response['message'], 'caught')
+
+    def test_returns_defined_status_on_error_exception_no_message(self):
+        """
+        Tests if wrapper returns status passed in argument.
+        """
+        fun = mock.Mock(side_effect=ValueError)
+
+        _, status = handle_error(ValueError, status=49)(fun)()
+
+        self.assertEqual(status, 49)
+
+    def test_returns_defined_message_on_error_exception_with_message(self):
+        """
+        Tests if wrapper returns error response with message equal to argument message,
+        when exception has an attribute named 'message'.
+        """
+        error = ValueError('error')
+        fun = mock.Mock(side_effect=error)
+
+        error_response, _ = handle_error(ValueError, message='caught')(fun)()
+
+        self.assertEqual(error_response['message'], 'caught')
+
+    def test_returns_exception_message_when_no_message_parameter(self):
+        """
+        Tests if wrapper returns error response with message equal to exception's message attribute,
+        when exception has an attribute named 'message'.
+        """
+        error = ValueError()
+        error.message = 'error'
+        fun = mock.Mock(side_effect=error)
+
+        error_response, _ = handle_error(ValueError)(fun)()
+
+        self.assertEqual(error_response['message'], 'error')
