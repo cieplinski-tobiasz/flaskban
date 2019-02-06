fbf57f5e8c4af2e287e00825c32bb224075835cf
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2019-02-06 15:17:09 +0100

    Add documentation to configurations

--- flaskban_server/config.py
+++ flaskban_server/config.py
@@ -8,6 +8,12 @@ Attributes:
 
 
 class Config:
+    """
+    Base class used for common configuration properties.
+
+    Properties:
+        SWAGGER (dict): Config of Flasgger extension.
+    """
     SWAGGER = {
         'title': 'FlaskBan',
         'uiversion': 3,
@@ -15,12 +21,32 @@ class Config:
 
 
 class DevelopmentConfig(Config):
+    """
+    Config used for development.
+
+    Properties:
+        SQLALCHEMY_DATABASE_URI (str): Address of the database server.
+        SQLALCHEMY_TRACK_MODIFICATIONS (bool): Configuration flag for SQLAlchemy.
+        JWT_SECRET_KEY (str): Key used for signing JWT tokens.
+    """
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'super-secret-developer-key'
 
 
 def get_config(name):
+    """
+    Returns config based on the name of it.
+
+    Args:
+        name (str): Name of the config.
+
+    Returns:
+        Config object.
+
+    Raises:
+        ValueError: If no config with given name exists.
+    """
     if name == 'dev':
         return DevelopmentConfig()
 
